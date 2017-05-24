effca1c7f07bb1c7e1b6a39587f41606e6a841b8
ctai <glaxy.tai@gmail.com>
2017-05-24 22:02:01 +0800

    label_image can recognize chinese character

--- tensorflow/examples/label_image/main.cc
+++ tensorflow/examples/label_image/main.cc
@@ -58,6 +58,7 @@ using tensorflow::Tensor;
 using tensorflow::Status;
 using tensorflow::string;
 using tensorflow::int32;
+using tensorflow::TensorShape;
 
 // Takes a file name, and loads a list of labels from it, one per line, and
 // returns a vector of the strings. It pads with empty strings so the length
@@ -85,6 +86,60 @@ Status ReadLabelsFile(string file_name, std::vector<string>* result,
 // Given an image file name, read in the data, try to decode it as an image,
 // resize it to the requested size, and then scale the values as desired.
 Status ReadTensorFromImageFile(string file_name, const int input_height,
+                               const int input_width, const float input_mean,
+                               const float input_std,
+                               std::vector<Tensor>* out_tensors) {
+  auto root = tensorflow::Scope::NewRootScope();
+  using namespace ::tensorflow::ops;  // NOLINT(build/namespaces)
+
+  string input_name = "file_reader";
+  string output_name = "normalized";
+  auto file_reader =
+      tensorflow::ops::ReadFile(root.WithOpName(input_name), file_name);
+  // Now try to figure out what kind of file it is and decode it.
+  const int wanted_channels = 1;
+  tensorflow::Output image_reader;
+  if (tensorflow::StringPiece(file_name).ends_with(".png")) {
+    image_reader = DecodePng(root.WithOpName("png_reader"), file_reader,
+                             DecodePng::Channels(wanted_channels));
+  } else if (tensorflow::StringPiece(file_name).ends_with(".gif")) {
+    image_reader = DecodeGif(root.WithOpName("gif_reader"), file_reader);
+  } else {
+    // Assume if it's neither a PNG nor a GIF then it must be a JPEG.
+    image_reader = DecodeJpeg(root.WithOpName("jpeg_reader"), file_reader,
+                              DecodeJpeg::Channels(wanted_channels));
+  }
+  // Now cast the image data to float so we can do normal math on it.
+  auto float_caster =
+      Cast(root.WithOpName("float_caster"), image_reader, tensorflow::DT_FLOAT);
+  // The convention for image ops in TensorFlow is that all images are expected
+  // to be in batches, so that they're four-dimensional arrays with indices of
+  // [batch, height, width, channel]. Because we only have a single image, we
+  // have to add a batch dimension of 1 to the start with ExpandDims().
+  auto dims_expander = ExpandDims(root, float_caster, 0);
+  // Bilinearly resize the image to fit the required dimensions.
+  auto resized = ResizeBilinear(
+      root, dims_expander,
+      Const(root.WithOpName("size"), {input_height, input_width}));
+  // Subtract the mean and divide by the scale.
+  Div(root.WithOpName(output_name), Sub(root, resized, {input_mean}),
+      {input_std});
+
+  // This runs the GraphDef network definition that we've just constructed, and
+  // returns the results in the output tensor.
+  tensorflow::GraphDef graph;
+  TF_RETURN_IF_ERROR(root.ToGraphDef(&graph));
+
+  std::unique_ptr<tensorflow::Session> session(
+      tensorflow::NewSession(tensorflow::SessionOptions()));
+  TF_RETURN_IF_ERROR(session->Create(graph));
+  TF_RETURN_IF_ERROR(session->Run({}, {output_name}, {}, out_tensors));
+  return Status::OK();
+}
+
+// Given an image file name, read in the data, try to decode it as an image,
+// resize it to the requested size, and then scale the values as desired.
+Status ReadTensorFromImageFile1(string file_name, const int input_height,
                                const int input_width, const float input_mean,
                                const float input_std,
                                std::vector<Tensor>* out_tensors) {
@@ -157,7 +212,7 @@ Status LoadGraph(string graph_file_name,
 
 // Analyzes the output of the Inception graph to retrieve the highest scores and
 // their positions in the tensor, which correspond to categories.
-Status GetTopLabels(const std::vector<Tensor>& outputs, int how_many_labels,
+Status GetTopLabels1(const std::vector<Tensor>& outputs, int how_many_labels,
                     Tensor* indices, Tensor* scores) {
   auto root = tensorflow::Scope::NewRootScope();
   using namespace ::tensorflow::ops;  // NOLINT(build/namespaces)
@@ -182,9 +237,70 @@ Status GetTopLabels(const std::vector<Tensor>& outputs, int how_many_labels,
   return Status::OK();
 }
 
+// Analyzes the output of the Inception graph to retrieve the highest scores and
+// their positions in the tensor, which correspond to categories.
+Status GetTopLabels(const std::vector<Tensor>& outputs, int how_many_labels,
+                    Tensor* indices, Tensor* scores) {
+//  auto root = tensorflow::Scope::NewRootScope();
+//  using namespace ::tensorflow::ops;  // NOLINT(build/namespaces)
+//
+//  string output_name = "top_k";
+//  TopK(root.WithOpName(output_name), outputs[0], how_many_labels);
+//  // This runs the GraphDef network definition that we've just constructed, and
+//  // returns the results in the output tensors.
+//  tensorflow::GraphDef graph;
+//  TF_RETURN_IF_ERROR(root.ToGraphDef(&graph));
+//
+//  std::unique_ptr<tensorflow::Session> session(
+//      tensorflow::NewSession(tensorflow::SessionOptions()));
+//  TF_RETURN_IF_ERROR(session->Create(graph));
+//  // The TopK node returns two outputs, the scores and their original indices,
+//  // so we have to append :0 and :1 to specify them both.
+//  std::vector<Tensor> out_tensors;
+//  TF_RETURN_IF_ERROR(session->Run({}, {output_name + ":0", output_name + ":1"},
+//                                  {}, &out_tensors));
+//  *scores = out_tensors[0];
+//  *indices = out_tensors[1];
+  *scores = outputs[0];
+  *indices = outputs[1];
+  return Status::OK();
+}
+
 // Given the output of a model run, and the name of a file containing the labels
 // this prints out the top five highest-scoring values.
 Status PrintTopLabels(const std::vector<Tensor>& outputs,
+                      string labels_file_name) {
+  std::vector<string> labels;
+  size_t label_count;
+  Status read_labels_status =
+      ReadLabelsFile(labels_file_name, &labels, &label_count);
+  if (!read_labels_status.ok()) {
+    LOG(ERROR) << read_labels_status;
+    return read_labels_status;
+  }
+  const int how_many_labels = 1;//std::min(5, static_cast<int>(label_count));
+
+  Tensor indices;
+  Tensor scores;
+  TF_RETURN_IF_ERROR(GetTopLabels(outputs, how_many_labels, &indices, &scores));
+  tensorflow::TTypes<float>::Flat scores_flat = scores.flat<float>();
+  LOG(INFO) << "scores"<<scores.DebugString();
+
+  tensorflow::TTypes<int32>::Flat indices_flat = indices.flat<int32>();
+  LOG(INFO) << "indices"<<indices.DebugString();
+
+  //  for (int pos = 0; pos < how_many_labels; ++pos) {
+//    const int label_index = indices_flat(pos);
+//    LOG(INFO) << label_index;
+////    const float score = scores_flat(pos);
+////    LOG(INFO) << labels[label_index] << " (" << label_index << "): " << score;
+//  }
+  return Status::OK();
+}
+
+// Given the output of a model run, and the name of a file containing the labels
+// this prints out the top five highest-scoring values.
+Status PrintTopLabels1(const std::vector<Tensor>& outputs,
                       string labels_file_name) {
   std::vector<string> labels;
   size_t label_count;
@@ -207,7 +323,6 @@ Status PrintTopLabels(const std::vector<Tensor>& outputs,
   }
   return Status::OK();
 }
-
 // This is a testing function that returns whether the top label index is the
 // one that's expected.
 Status CheckTopLabel(const std::vector<Tensor>& outputs, int expected,
@@ -229,6 +344,127 @@ Status CheckTopLabel(const std::vector<Tensor>& outputs, int expected,
 }
 
 int main(int argc, char* argv[]) {
+  // These are the command-line flags the program can understand.
+  // They define where the graph and input data is located, and what kind of
+  // input the model expects. If you train your own model, or use something
+  // other than inception_v3, then you'll need to update these.
+  string image = "tensorflow/examples/label_image/data/31-70110.png";
+  string graph =
+      "tensorflow/examples/label_image/data/forzen-chinese-rec-model4501.0.pb";
+  string labels =
+      "tensorflow/examples/label_image/data/imagenet_slim_labels.txt";
+  int32 input_width = 64;
+  int32 input_height = 64;
+  int32 input_mean = 0;
+  int32 input_std = 255;
+  string input_layer = "image_batch";
+  string output_layer = "top_k";
+  bool self_test = false;
+  string root_dir = "";
+  std::vector<Flag> flag_list = {
+      Flag("image", &image, "image to be processed"),
+      Flag("graph", &graph, "graph to be executed"),
+      Flag("labels", &labels, "name of file containing labels"),
+      Flag("input_width", &input_width, "resize image to this width in pixels"),
+      Flag("input_height", &input_height,
+           "resize image to this height in pixels"),
+      Flag("input_mean", &input_mean, "scale pixel values to this mean"),
+      Flag("input_std", &input_std, "scale pixel values to this std deviation"),
+      Flag("input_layer", &input_layer, "name of input layer"),
+      Flag("output_layer", &output_layer, "name of output layer"),
+      Flag("self_test", &self_test, "run a self test"),
+      Flag("root_dir", &root_dir,
+           "interpret image and graph file names relative to this directory"),
+  };
+  string usage = tensorflow::Flags::Usage(argv[0], flag_list);
+  const bool parse_result = tensorflow::Flags::Parse(&argc, argv, flag_list);
+  if (!parse_result) {
+    LOG(ERROR) << usage;
+    return -1;
+  }
+
+  // We need to call this to set up global state for TensorFlow.
+  tensorflow::port::InitMain(argv[0], &argc, &argv);
+  if (argc > 1) {
+    LOG(ERROR) << "Unknown argument " << argv[1] << "\n" << usage;
+    return -1;
+  }
+
+  // First we load and initialize the model.
+  std::unique_ptr<tensorflow::Session> session;
+  string graph_path = tensorflow::io::JoinPath(root_dir, graph);
+  Status load_graph_status = LoadGraph(graph_path, &session);
+  if (!load_graph_status.ok()) {
+    LOG(ERROR) << load_graph_status;
+    return -1;
+  }
+  printf("\nLoadGraph success!\n");
+  // Get the image from disk as a float array of numbers, resized and normalized
+  // to the specifications the main graph expects.
+  std::vector<Tensor> resized_tensors;
+  string image_path = tensorflow::io::JoinPath(root_dir, image);
+  Status read_tensor_status =
+      ReadTensorFromImageFile(image_path, input_height, input_width, input_mean,
+                              input_std, &resized_tensors);
+  if (!read_tensor_status.ok()) {
+    LOG(ERROR) << read_tensor_status;
+    return -1;
+  }
+  const Tensor& resized_tensor = resized_tensors[0];
+
+//  Tensor label(tensorflow::DT_INT64, TensorShape({1}));
+//  auto label_vec = label.vec<long long>();
+//  label_vec(0) = {31};
+//  string label_layer = "label_batch";
+
+  Tensor keep_prob(tensorflow::DT_FLOAT, TensorShape({1}));
+  auto keep_prob_vec = keep_prob.vec<float>();
+  keep_prob_vec(0) = {1};
+  string keep_prob_layer = "keep_prob";
+
+  Tensor train_flag(tensorflow::DT_BOOL, TensorShape({}));
+  train_flag.scalar<bool>()()=false;
+  string train_flag_layer = "train_flag";
+
+  // Actually run the image through the model.
+  std::vector<Tensor> outputs;
+//  Status run_status = session->Run({{input_layer, resized_tensor}, {label_layer, label}, {keep_prob_layer, keep_prob}, {train_flag_layer, train_flag}},
+//                                   {output_layer}, {}, &outputs);
+  Status run_status = session->Run({{input_layer, resized_tensor}, {keep_prob_layer, keep_prob}, {train_flag_layer, train_flag}},
+                                   { {output_layer + ":0", output_layer + ":1"}}, {}, &outputs);
+  if (!run_status.ok()) {
+    LOG(ERROR) << "Running model failed: " << run_status;
+    return -1;
+  }
+  LOG(INFO) << keep_prob.DebugString();
+//  // This is for automated testing to make sure we get the expected result with
+//  // the default settings. We know that label 653 (military uniform) should be
+//  // the top label for the Admiral Hopper image.
+//  if (self_test) {
+//    bool expected_matches;
+//    Status check_status = CheckTopLabel(outputs, 31, &expected_matches);
+//    if (!check_status.ok()) {
+//      LOG(ERROR) << "Running check failed: " << check_status;
+//      return -1;
+//    }
+//    if (!expected_matches) {
+//      LOG(ERROR) << "Self-test failed!";
+//      return -1;
+//    }
+//  }
+
+  // Do something interesting with the results we've generated.
+  Status print_status = PrintTopLabels(outputs, labels);
+  if (!print_status.ok()) {
+    LOG(ERROR) << "Running print failed: " << print_status;
+    return -1;
+  }
+
+  return 0;
+}
+
+
+int main1(int argc, char* argv[]) {
   // These are the command-line flags the program can understand.
   // They define where the graph and input data is located, and what kind of
   // input the model expects. If you train your own model, or use something
@@ -289,7 +525,7 @@ int main(int argc, char* argv[]) {
   std::vector<Tensor> resized_tensors;
   string image_path = tensorflow::io::JoinPath(root_dir, image);
   Status read_tensor_status =
-      ReadTensorFromImageFile(image_path, input_height, input_width, input_mean,
+      ReadTensorFromImageFile1(image_path, input_height, input_width, input_mean,
                               input_std, &resized_tensors);
   if (!read_tensor_status.ok()) {
     LOG(ERROR) << read_tensor_status;
